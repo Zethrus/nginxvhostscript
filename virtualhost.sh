f6c716ced9cf0364ff2ee411dc5e35ee0a3aacbc
Zethrus <zethrustv@gmail.com>
2015-05-15 18:06:07 -0600

    Stupid characters, go do your stuff

--- virtualhost.sh
+++ virtualhost.sh
@@ -91,10 +91,10 @@ Please check permissions."
                         error_page 404 /index.php;
 
                         location ~ \.php$ {
-                                try_files $uri =404;
+                                try_files \$uri =404;
                                 fastcgi_pass unix:/var/run/php5-fpm.sock;
                                 fastcgi_index index.php;
-                                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                                fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
                                 include fastcgi_params;
                         }
 
